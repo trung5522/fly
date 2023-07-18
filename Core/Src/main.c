e385f7ff3d131ffa41180bf09c224ee8f21f079f
trung5522 <trung.pham0550@hcmut.edu.vn>
2023-07-18 11:09:54 +0700

    18/7

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -145,6 +145,41 @@ int disable_motor = 1000;
 
 float turning_speed = 5.0;
 bool auto_level = true;
+
+//float KalmanOut[2];
+/* Kalman filter variables */
+float Q_anglez= 0.008f;; // Process noise variance for the accelerometer
+float Q_biasz= 0.003f; // Process noise variance for the gyro bias
+float R_measurez= 0.05f; // Measurement noise variance - this is actually the variance of the measurement noise
+
+float Q_angle= 0.008f; // Process noise variance for the accelerometer
+float Q_bias= 0.003f; // Process noise variance for the gyro bias
+float R_measure= 0.05f;// Measurement noise variance - this is actually the variance of the measurement noise
+
+
+
+float angle = 0.0f;; // The angle calculated by the Kalman filter - part of the 2x1 state vector
+float bias =0.0f;; // The gyro bias calculated by the Kalman filter - part of the 2x1 state vector
+float rate; // Unbiased rate calculated from the rate and the calculated bias - you have to call getAngle to update the rate
+
+float P[2][2]; // Error covariance matrix - This is a 2x2 matrix
+float S;
+float K[2];
+float y;
+float P00_temp;
+float P01_temp;
+
+
+float anglez = 0.0f; // The angle calculated by the Kalman filter - part of the 2x1 state vector
+float biasz =0.0f;// The gyro bias calculated by the Kalman filter - part of the 2x1 state vector
+float ratez; // Unbiased rate calculated from the rate and the calculated bias - you have to call getAngle to update the rate
+
+float Pz[2][2]; // Error covariance matrix - This is a 2x2 matrix
+float Sz;
+float Kz[2];
+float yz;
+float P00_tempz;
+float P01_tempz;
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -161,6 +196,110 @@ TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
+float Kalman_getAngle_roll(float newAngle, float newRate, float dt) {
+    // KasBot V2  -  Kalman filter module - http://www.x-firm.com/?page_id=145
+    // Modified by Kristian Lauszus
+    // See my blog post for more information: http://blog.tkjelectronics.dk/2012/09/a-practical-approach-to-kalman-filter-and-how-to-implement-it
+
+    // Discrete Kalman filter time update equations - Time Update ("Predict")
+    // Update xhat - Project the state ahead
+    /* Step 1 */
+   rate = newRate - bias;
+    angle += dt * rate;
+
+    // Update estimation error covariance - Project the error covariance ahead
+    /* Step 2 */
+    P[0][0] += dt * (dt*P[1][1] - P[0][1] - P[1][0] + Q_angle);
+    P[0][1] -= dt * P[1][1];
+    P[1][0] -= dt * P[1][1];
+    P[1][1] += (Q_bias) * dt;
+
+    // Discrete Kalman filter measurement update equations - Measurement Update ("Correct")
+    // Calculate Kalman gain - Compute the Kalman gain
+    /* Step 4 */
+     S = P[0][0] + R_measure; // Estimate error
+    /* Step 5 */
+     // Kalman gain - This is a 2x1 vector
+    K[0] = P[0][0] / S;
+    K[1] = P[1][0] / S;
+
+    // Calculate angle and bias - Update estimate with measurement zk (newAngle)
+    /* Step 3 */
+     y = newAngle - angle; // Angle difference
+    /* Step 6 */
+    angle += K[0] * y;
+    bias += K[1] * y;
+
+    // Calculate estimation error covariance - Update the error covariance
+    /* Step 7 */
+    P00_temp = P[0][0];
+    P01_temp = P[0][1];
+
+    P[0][0] -= K[0] * P00_temp;
+    P[0][1] -= K[0] * P01_temp;
+    P[1][0] -= K[1] * P00_temp;
+    P[1][1] -= K[1] * P01_temp;
+
+    return angle;
+};
+//
+float Kalman_getAngle_pitch(float newAnglez, float newRatez, float dt) {
+    // KasBot V2  -  Kalman filter module - http://www.x-firm.com/?page_id=145
+    // Modified by Kristian Lauszus
+    // See my blog post for more information: http://blog.tkjelectronics.dk/2012/09/a-practical-approach-to-kalman-filter-and-how-to-implement-it
+
+    // Discrete Kalman filter time update equations - Time Update ("Predict")
+    // Update xhat - Project the state ahead
+    /* Step 1 */
+    ratez = newRatez - biasz;
+    anglez += dt * ratez;
+
+    // Update estimation error covariance - Project the error covariance ahead
+    /* Step 2 */
+    Pz[0][0] += dt * (dt*Pz[1][1] - Pz[0][1] - Pz[1][0] + Q_anglez);
+    Pz[0][1] -= dt * Pz[1][1];
+    Pz[1][0] -= dt * Pz[1][1];
+    Pz[1][1] += (Q_biasz ) * dt;
+
+    // Discrete Kalman filter measurement update equations - Measurement Update ("Correct")
+    // Calculate Kalman gain - Compute the Kalman gain
+    /* Step 4 */
+     Sz = Pz[0][0] + R_measurez; // Estimate error
+    /* Step 5 */
+     // Kalman gain - This is a 2x1 vector
+    Kz[0] = Pz[0][0] / Sz;
+    Kz[1] = Pz[1][0] / Sz;
+
+    // Calculate angle and bias - Update estimate with measurement zk (newAngle)
+    /* Step 3 */
+     yz = newAnglez - anglez; // Angle difference
+    /* Step 6 */
+    anglez += Kz[0] * yz;
+    biasz += Kz[1] * yz;
+
+    // Calculate estimation error covariance - Update the error covariance
+    /* Step 7 */
+    P00_tempz = Pz[0][0];
+    P01_tempz = Pz[0][1];
+
+    Pz[0][0] -= Kz[0] * P00_tempz;
+    Pz[0][1] -= Kz[0] * P01_tempz;
+    Pz[1][0] -= Kz[1] * P00_tempz;
+    Pz[1][1] -= Kz[1] * P01_tempz;
+
+    return anglez;
+};
+//void kalman(float KalmanState,float KalmanUncer,float KalmanInput, float KalmanMeasure){
+//	KalmanState = KalmanState +0.007*KalmanInput;
+//	KalmanUncer = KalmanUncer +0.007*0.007*4*4;
+//	float KalmanGain = KalmanUncer*1/(1*KalmanUncer+3*3);
+//	KalmanState = KalmanState + KalmanGain*(KalmanMeasure-KalmanState);
+//	KalmanUncer = (1-KalmanGain)*KalmanUncer;
+//	KalmanOut[0]=KalmanState;
+//	KalmanOut[1]=KalmanUncer;
+//}
+
+
 void HAL_GPIO_EXTI_Callback (uint16_t GPIO_Pin){
 	if(GPIO_Pin == GPIO_PIN_11){
 		tick = __HAL_TIM_GET_COUNTER(&htim4);
@@ -397,8 +536,8 @@ int main(void)
 //	 	 angle_pitch_acc_1 -= 0.57;		// -0.08
 //	 	 angle_roll_acc_1 -= -0.427;		// -1
 //
-	 	angle_pitch_acc = (-(mpu.rpy[1] + 0.15));
-	 	angle_roll_acc = (mpu.rpy[0] + 0.3);
+	 	angle_pitch_acc = Kalman_getAngle_pitch((-(mpu.rpy[1] + 1.57)), mpu.g[1], 0.006);//(-(mpu.rpy[1] + 1.42));
+	 	angle_roll_acc = Kalman_getAngle_roll((mpu.rpy[0] + 0.38), mpu.g[0], 0.006);//(mpu.rpy[0] + 0.38);
 
 	 	if ( set_gyro_angle ) {
 	 		angle_pitch = angle_pitch * 0.8 + angle_pitch_acc * 0.2;
